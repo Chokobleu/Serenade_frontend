accf368e792eb3473906ead4f2d2e8fb5aa85750
Sylvie Helin <sylviepil1l1@gmail.com>
2023-05-25 13:15:05 +0200

    ajout condition texte non vide pour addmessage

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -45,90 +45,6 @@ const ChatScreen = () => {
 
 
   const [messages, setMessages] = useState([])
-  //   {
-  //     firstname: "Alice",
-  //     message: "Salut Bob, comment vas-tu ?",
-  //     date: "2022-04-10T14:30:00Z",
-  //   },
-  //   {
-  //     firstname: "Bob",
-  //     message: "Salut Alice, ça va bien, merci ! Et toi ?",
-  //     date: "2022-04-10T14:32:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message: "Je vais bien aussi, merci. ",
-  //     date: "2022-04-10T14:35:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message: "Quoi de neuf ?",
-  //     date: "2022-04-10T14:35:00Z",
-  //   },
-  //   {
-  //     firstname: "Bob",
-  //     message: "Pas grand-chose, et toi ?",
-  //     date: "2022-04-10T14:38:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message: "Rien de spécial non plus. Tu as des projets pour ce week-end ?",
-  //     date: "2022-04-10T14:40:00Z",
-  //   },
-  //   {
-  //     firstname: "Bob",
-  //     message:
-  //       "Pas vraiment, je pense que je vais rester à la maison. Et toi ?",
-  //     date: "2022-04-11T14:42:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message:
-  //       "Je vais probablement sortir avec des amis samedi soir. Tu veux te joindre à nous ?",
-  //     date: "2022-04-20T14:45:00Z",
-  //   },
-  //   {
-  //     firstname: "Bob",
-  //     message: "Je ne sais pas encore, je te tiendrai au courant. Tu vas où ?",
-  //     date: "2022-04-20T14:48:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message: "On va au restaurant italien près de chez moi, à 20h.",
-  //     date: "2022-04-20T14:50:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message: "On va au restaurant italien près de chez moi, à 20h.",
-  //     date: "2022-04-20T14:50:00Z",
-  //   },
-  //   {
-  //     firstname: "Bob",
-  //     message: "OK, je te tiens au courant. À plus tard !",
-  //     date: "2022-04-20T14:52:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message: "À plus tard !",
-  //     date: "2022-04-20T14:53:00Z",
-  //   },
-  //   {
-  //     firstname: "Bob",
-  //     message:
-  //       "Salut Alice, finalement je ne pourrai pas venir ce soir. Désolé.",
-  //     date: "2022-04-21T10:00:00Z",
-  //   },
-  //   {
-  //     firstname: "Alice",
-  //     message: "Pas de soucis, une prochaine fois alors !",
-  //     date: "2022-04-21T10:02:00Z",
-  //   },
-  //   {
-  //     firstname: "Bob",
-  //     message: "Oui, absolument. Tu me raconteras comment c'était.",
-  //     date: "2022-04-21T10:15:00Z",
-  //   },
-  // ]);
 
   // Diplay end of scrollview at the first opening
   //-------------------------------------------------------------
@@ -145,6 +61,8 @@ const loadMessages=()=>{
       setMessages(data.messages)
 
     });
+
+
 
 }
 
@@ -163,6 +81,10 @@ const loadMessages=()=>{
     setIsScrollViewAtEnd(true);
   };
 
+  // setTimeout(() =>{
+  //   loadMessages()
+  // },2000)
+
   //--------------------------------------------------------------
   // const avatarImage =
   //   "https://images.pexels.com/photos/1382726/pexels-photo-1382726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
@@ -170,12 +92,11 @@ const loadMessages=()=>{
   // Send messages
   //--------------------------------------------------------------
 
-  // const conversationId = "";
-  // const userId = "";
-  // const recipientId = "";
-  const date = new Date();
 
   const handleSendMessage = () => {
+
+    if(messageText){
+    const date = new Date();
 
     userToken = token
 
@@ -183,7 +104,8 @@ const loadMessages=()=>{
       author: token,
       content: messageText,
       date: date
-    };
+    }
+    
 
 
     fetch(`${url}/users/chat/addMessage`, {
@@ -199,6 +121,7 @@ const loadMessages=()=>{
         }
 
       }); setMessageText("");
+    };
   };
 
   // Dislike
